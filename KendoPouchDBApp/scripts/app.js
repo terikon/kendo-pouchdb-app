b3e88867015bc9cae29adf4b02d0be0947e073c2
viskin <r.viskin@terikon.com>
2015-04-27 13:52:04 +0300

    Added contacts.html page. Initialized Couchbase mobile.

--- KendoPouchDBApp/scripts/app.js
+++ KendoPouchDBApp/scripts/app.js
@@ -7,41 +7,55 @@
 
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        home: {
-          title: 'Home'
-        },
-        settings: {
-          title: 'Settings'
-        },
-        contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+        models: {
+            home: {
+                title: 'Home'
+            },
+            contacts: {
+                title: 'Contacts',
+                ds: new kendo.data.DataSource({
+                    data: [{ href: "mailto:npm@terikon.com?subject=Question about KendoPouchDBApp", name: 'email npm@terikon.com' }, { href: "https://github.com/terikon/kendo-pouchdb", name: 'kendo-pouchdb homepage' }]
+                }),
+                open: function (e) {
+                    window.open(e.data.href, "_system");
+                }
+            }
         }
-      }
+    };
+
+    var couchbaseSetup = function () {
+        window.cblite.getURL(function (err, url) {
+            if (err) {
+                alert("CouchbaseLite Initilization error " + JSON.stringify(err))
+            }
+            alert("url:" + url);
+        });
+    };
+
+    var init = function () {
+
     };
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        couchbaseSetup();
+
+        app = new kendo.mobile.Application(document.body, {
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/home.html',
+
+            init: init
+        });
 
     }, false);
 
